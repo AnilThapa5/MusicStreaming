e03071ff00142480fde338e1e8713fc6785a6255
AnilThapa5 <thapaanil307@gmail.com>
2021-01-08 12:58:55 +0545

    blog added

--- music/app/models.py
+++ music/app/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 
 class Blog(models.Model):
-
     img = models.ImageField(upload_to='pics')
     title = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
     comment = models.TextField()
     desc = models.TextField()
+
+
--- music/app/views.py
+++ music/app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Blog
+
+
 # Create your views here.
 
 
@@ -9,12 +11,6 @@ def index(request):
     return render(request, 'index.html')
 
 
-class Blogs(object):
-    pass
-
-
 def blog(request):
-
     blog1 = Blog.objects.all()
-
     return render(request, "blog.html", {'blog1': blog1})
