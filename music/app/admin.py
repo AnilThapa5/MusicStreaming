597fab971cdc4bf0cdac99938833106caf942b7b
AnilThapa5 <thapaanil307@gmail.com>
2021-01-10 17:55:56 +0545

    song class added

--- music/app/admin.py
+++ music/app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from . models import Blog
+from . models import Blog,Song
 
 
 # Register your models here.
 
 admin.site.register(Blog)
+admin.site.register(Song)
--- music/app/models.py
+++ music/app/models.py
@@ -11,3 +11,21 @@ class Blog(models.Model):
     desc = models.TextField()
 
 
+class Song(models.Model):
+
+    genreChoices = [
+        ('PP', 'POP'),
+        ('RK', 'ROCK'),
+        ('CL', 'CLASSIC'),
+        ('ML', 'METAL')
+    ]
+    img = models.ImageField(upload_to='pics')
+    title = models.TextField(max_length=30)
+    desc = models.TextField(max_length=30)
+    date = models.DateField(auto_now=True)
+    genres = models.CharField(max_length=2, choices=genreChoices)
+    songs = models.FileField(upload_to='music/')
+    artist = models.TextField(max_length=30)
+
+
+
