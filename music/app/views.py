fbf018b6dbfcc56beb18ccd8e4e54906aa6ece4e
AnilThapa5 <thapaanil307@gmail.com>
2021-01-13 07:52:57 +0545

    music player added and fetch

--- music/app/views.py
+++ music/app/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from .models import Blog
+from .models import Blog, Song
 
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    song1 = Song.objects.all()
+    return render(request, "index.html", {'song1': song1})
 
 
 def blog(request):
     blog1 = Blog.objects.all()
     return render(request, "blog.html", {'blog1': blog1})
+
+
+
+
